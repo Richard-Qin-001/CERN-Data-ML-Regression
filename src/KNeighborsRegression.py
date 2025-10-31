d9e13f8d9e808023c81cca1af2365361f229c560
Richard-Qin-001 <69390919+Richard-Qin-001@users.noreply.github.com>
2025-10-31 20:02:03 +0800

    KNeighbor Regression

---
+++ src/KNeighborsRegression.py
@@ -0,0 +1,26 @@
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+from LinearRegression import data_split
+from data_prep import load_data_from_db
+from visualization import visualization_knn
+
+def train_knn(X_train_scaled, X_test_scaled, y_train, y_test):
+    n_neighbors = 5
+    knn_model = KNeighborsRegressor(n_neighbors)
+    print(f"\nStart training the KNN regression model (K={n_neighbors})...")
+    knn_model.fit(X_train_scaled, y_train)
+    y_pred_knn = knn_model.predict(X_test_scaled)
+
+    mse_knn = mean_squared_error(y_test, y_pred_knn)
+    r2_knn = r2_score(y_test, y_pred_knn)
+
+    print(f"\nKNN Regression Model Performance (K={n_neighbors})")
+    print(f"MSE: {mse_knn}")
+    print(f"R^2: {r2_knn}")
+    return y_pred_knn
+
+if __name__ == '__main__':
+    df = load_data_from_db()
+    X_train_scaled, X_test_scaled, y_train, y_test = data_split(df)
+    y_pred_knn = train_knn(X_train_scaled, X_test_scaled, y_train, y_test)
+    visualization_knn(y_test, y_pred_knn)
--- src/visualization.py
+++ src/visualization.py
@@ -78,14 +78,30 @@ def draw_chart(df):
 
 def visualization_linear_regression(y_test, y_pred_linear):
     plt.figure(figsize=(8, 8))
-    plt.scatter(y_test, y_pred_linear, alpha=0.3, s=10, label='Predicted vs. Actual')
+    plt.scatter(y_test, y_pred_linear, alpha=0.3, s=10, label='Predicted & Actual')
 
     max_val = max(y_test.max(), y_pred_linear.max())
     min_val = min(y_test.min(), y_pred_linear.min())
     plt.plot([min_val, max_val], [min_val, max_val], 
              color='red', linestyle='--', linewidth=2, label='Ideal Prediction (y=x)')
 
-    plt.title("Linear Regression: Predicted vs. Actual Invariant Mass (M)", fontsize=14)
+    plt.title("Linear Regression: Predicted & Actual Invariant Mass (M)", fontsize=14)
+    plt.xlabel("Actual Invariant Mass (GeV)", fontsize=12)
+    plt.ylabel("Predicted Invariant Mass (GeV)", fontsize=12)
+    plt.legend(fontsize=10)
+    plt.grid(True, linestyle=':', alpha=0.6)
+    plt.show()
+
+def visualization_knn(y_test, y_pred_knn):
+    plt.figure(figsize=(8, 8))
+    plt.scatter(y_test, y_pred_knn, alpha=0.3, s=10, label='Predicted & Actual')
+
+    max_val = max(y_test.max(), y_pred_knn.max())
+    min_val = min(y_test.min(), y_pred_knn.min())
+    plt.plot([min_val, max_val], [min_val, max_val], 
+             color='red', linestyle='--', linewidth=2, label='Ideal Prediction (y=x)')
+
+    plt.title("KNeighbors Regression: Predicted & Actual Invariant Mass (M)", fontsize=14)
     plt.xlabel("Actual Invariant Mass (GeV)", fontsize=12)
     plt.ylabel("Predicted Invariant Mass (GeV)", fontsize=12)
     plt.legend(fontsize=10)
